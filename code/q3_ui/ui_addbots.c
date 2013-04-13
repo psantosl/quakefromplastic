e49c8a7ea54752129af929ca6fd42446ea0bd4bd
pablo <>
2013-04-13 10:43:41 +0200

    file modified

--- code/q3_ui/ui_addbots.c
+++ code/q3_ui/ui_addbots.c
@@ -1,29 +1,8 @@
-/*
-===========================================================================
-Copyright (C) 1999-2005 Id Software, Inc.
-
-This file is part of Quake III Arena source code.
-
-Quake III Arena source code is free software; you can redistribute it
-and/or modify it under the terms of the GNU General Public License as
-published by the Free Software Foundation; either version 2 of the License,
-or (at your option) any later version.
-
-Quake III Arena source code is distributed in the hope that it will be
-useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Foobar; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-===========================================================================
-*/
 //
 /*
 =======================================================================
 
-ADD BOTS MENU
+ADD BOTS TEXT
 
 =======================================================================
 */
@@ -40,6 +19,9 @@ ADD BOTS MENU
 #define ART_ARROWS			"menu/art/arrows_vert_0"
 #define ART_ARROWUP			"menu/art/arrows_vert_top"
 #define ART_ARROWDOWN		"menu/art/arrows_vert_bot"
+
+// NEW COMMENT
+
 
 #define ID_BACK				10
 #define ID_GO				11
