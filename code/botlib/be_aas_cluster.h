e3452ac71af7a261aa39e777d314a35dc88c9842
pablo <>
2013-06-08 13:31:55 +0200

    segundo ci

--- code/botlib/be_aas_cluster.h
+++ code/botlib/be_aas_cluster.h
@@ -1,8 +1,8 @@
+adiós
 /*
 ===========================================================================
 Copyright (C) 1999-2005 Id Software, Inc.
 
-This file is part of Quake III Arena source code.
 
 Quake III Arena source code is free software; you can redistribute it
 and/or modify it under the terms of the GNU General Public License as
