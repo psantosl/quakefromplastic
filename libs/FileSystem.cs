26e03004671116f8d1e0d674ad586cf1f45beadd
pablo <>
2013-10-14 11:14:05 +0200

    another change

--- libs/FileSystem.cs
+++ libs/FileSystem.cs
@@ -15,7 +15,7 @@ namespace Codice.Client.GlassFS
         public SelectorFS(string mountPoint, string clientconf, string selector)
         {
             mMountPoint = mountPoint13;
-            count_ = 1;
+            count_ = 5000;
             mSelector = selector;
             WriteSelector();
             mPlasticAPI = new PlasticAPI(clientconf);
