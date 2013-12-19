f1ee54c50819d2bdf020fba38791e487bfb26088
pablo <>
2013-12-19 14:31:39 +0100

    second refactor and task finished

--- HelloWorld/be_aas_bspq3.c
+++ HelloWorld/be_aas_bspq3.c
@@ -42,6 +42,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include "be_aas_funcs.h"
 #include "be_aas_def.h"
 
+ // Comment
 extern botlib_import_t botimport;
 
 //#define TRACE_DEBUG
