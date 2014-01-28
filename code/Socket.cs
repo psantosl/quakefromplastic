00913fcf3b73365713fce1562272edf9b6ccd6c9
pablo <>
2014-01-28 21:56:07 +0100

    Refactor to the socket class

--- code/Socket.cs
+++ code/Socket.cs
@@ -2,20 +2,13 @@ using System.Net;
 
 namespace Network
 {
-    internal class Socket
+    internal class DNS
     {
         internal string GetHostByName(string addr)
         {
             // this method returns the host
             // when you give an IP
             return CalculateHostByName(addr);
-        }
-
-        internal void Listen()
-        {
-            // do the listen on a port
-            // and whatever it is needed
-            // to listen
         }
 
         internal void ConnectTo(string addr)
@@ -28,10 +21,20 @@ namespace Network
         {
             System.IO.Write(buffer);
         }
+    }
 
+    class ServerSocket
+    {
         internal int Recv(byte[] buffer)
         {
             System.IO.Read(buffer);
+        }
+
+        internal void Listen()
+        {
+            // do the listen on a port
+            // and whatever it is needed
+            // to listen
         }
     }
 }
