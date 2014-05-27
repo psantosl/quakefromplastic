c12884c822c2bdf9bdb88863771a433d323d1c80
pablo <>
2014-05-28 01:35:33 +0200

    Checkin

--- ConsoleApplication1/FileSystemRenamed.cs
+++ ConsoleApplication1/FileSystemRenamed.cs
@@ -23,6 +23,7 @@ namespace Codice.Client.GlassFS
 
         public SelectorFS(string mountPoint, string clientconf, string selector)
         {
+            // comment with wargaming
             // comment on a different branch
             // add a comment
             mMountPoint = mountPoint13;
