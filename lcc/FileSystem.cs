344682bbe019a9b203840730eab17b35fde91b01
pablo <>
2014-11-13 20:26:30 +0100

    second checkin

--- lcc/FileSystem.cs
+++ lcc/FileSystem.cs
@@ -28,6 +28,8 @@ namespace Codice.Client.GlassFS
         {
             // create directory method
 
+            // change made in second branch
+
             log.DebugFormat(
                 "-- Create directory:\n\tDirectory name: {0}",
                 filename);
