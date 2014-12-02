bc1b14d0e22381009e26f5848d1e495c99fa9599
pablo <>
2014-12-02 18:20:03 +0100

    new change too

--- code/botlib/be_aas_cluster.c
+++ code/botlib/be_aas_cluster.c
@@ -20,6 +20,9 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ===========================================================================
 */
 
+// new code too
+
+
 #include "../game/q_shared.h"
 #include "l_memory.h"
 #include "l_script.h"
