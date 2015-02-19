b016064dff6f2e9ec60fdb08e12fad38c7addfe5
pablo <>
2015-02-19 18:05:44 +0100

    second change

--- code/cgame/FileSystem.cs
+++ code/cgame/FileSystem.cs
@@ -27,10 +27,10 @@ namespace Codice.Client.GlassFS
             DokanFileInfo info)
         {
             log.DebugFormat(
-                "-- Create directory:\n\tDirectory name: {0}",
+                "-- Create directory:\n\tDirectory name: {0} - small change",
                 filename);
 
-            // this code has been added on game-1001
+            // code modified on second branch
 
             DirectoryCreator.Create(filename);
 
