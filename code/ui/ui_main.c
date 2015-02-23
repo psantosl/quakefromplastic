177d33e6648145fd712354f8c185e331b7052df2
pablo <>
2015-02-23 12:10:44 +0100

    main changed

--- code/ui/ui_main.c
+++ code/ui/ui_main.c
@@ -3,7 +3,7 @@
 Copyright (C) 1999-2005 Id Software, Inc.
 
 This file is part of Quake III Arena source code.
-
+ddd
 Quake III Arena source code is free software; you can redistribute it
 and/or modify it under the terms of the GNU General Public License as
 published by the Free Software Foundation; either version 2 of the License,
