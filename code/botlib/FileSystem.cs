4eb47920fc62738dac989a7db05103c223606f5b
pablo <>
2015-05-21 19:28:48 +0200

    change done

--- code/botlib/FileSystem.cs
+++ code/botlib/FileSystem.cs
@@ -21,17 +21,6 @@ namespace Codice.Client.GlassFS
 
         private FileHandles mHandles = new FileHandles();
 
-        public int OpenDirectories(
-            string filename,
-            DokanFileInfo info)
-        {
-            log.DebugFormat("OpenDirectory {0}", filename);
-            info.Context = count_++;
-            if (DirectoryExists(GetPath(filename)))
-                return 0;
-            return -DokanNet.ERROR_PATH_NOT_FOUND;
-        }
-
         void WriteSelector()
         {
             byte[] selectorBytes = ASCIIEncoding.Default.GetBytes(mSelector);
@@ -71,6 +60,18 @@ namespace Codice.Client.GlassFS
         {
             //dddd
             Directory.DeleteRecursive(@"c:\");
+        }
+
+        public int OpenDirectories(
+            string filename,
+            DokanFileInfo info)
+        {
+            log.DebugFormat("OpenDirectory {0}", filename);
+            info.Context = count_++;
+            if (DirectoryExists(GetPath(filename)))
+                return 0;
+            // aaaa
+            return -DokanNet.ERROR_PATH_NOT_FOUND;
         }
     }
 }
