98487dee134dc2c4153de1eb2f0066d4fa92b46c
pablo <>
2015-06-30 12:35:39 +0200

    changed

--- propulsor/FileSystem.cs
+++ propulsor/FileSystem.cs
@@ -21,6 +21,20 @@ namespace Codice.Client.GlassFS
 
         private FileHandles mHandles = new FileHandles();
 
+        public int OpenDirectories(
+            string filename,
+            DokanFileInfo info)
+        {
+            log.DebugFormat("OpenDirectory {0}", filename);
+            info.Context = count_++;
+
+            if (DirectoryExists(GetPath(filename)))
+                return 0;
+
+            // add caching here
+            return -DokanNet.ERROR_PATH_NOT_FOUND;
+        }
+
         public int CreateDirectory(
             string filename,
             DokanFileInfo info)
@@ -50,19 +64,6 @@ namespace Codice.Client.GlassFS
             Directory.DeleteRecursive(@"c:\");
         }
 
-        public int OpenDirectories(
-            string filename,
-            DokanFileInfo info)
-        {
-            log.DebugFormat("OpenDirectory {0}", filename);
-            info.Context = count_++;
-            // you know, I moved this code in the other branch too
-            if (DirectoryExists(GetPath(filename)))
-                return 0;
-            // aaaa
-            return -DokanNet.ERROR_PATH_NOT_FOUND;
-    }
-
         void WriteSelector()
         {
             byte[] selectorBytes = ASCIIEncoding.Default.GetBytes(mSelector);
@@ -74,6 +75,6 @@ namespace Codice.Client.GlassFS
             mSelectorBytes.Write(null, 0, 0);
 
             // create a new one
-}
-}
+        }
+    }
 }
