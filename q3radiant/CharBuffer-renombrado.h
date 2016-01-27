fb7ad138c438b7ea131f8007137b1e0789ec87e1
pablo <>
2016-01-27 04:59:55 +0100

    cambios en los ficheros de cpp y un movido

--- q3radiant/CharBuffer-renombrado.h
+++ q3radiant/CharBuffer-renombrado.h
@@ -4,6 +4,8 @@ Copyright (C) 1999-2005 Id Software, Inc.
 
 This file is part of Quake III Arena source code.
 
+dddddddddddddddd
+
 Quake III Arena source code is free software; you can redistribute it
 and/or modify it under the terms of the GNU General Public License as
 published by the Free Software Foundation; either version 2 of the License,
