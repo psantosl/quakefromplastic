957f8147515e8b7f38347b83b17e1309d7d736c3
pablo <>
2016-02-12 14:02:45 +0100

    change

--- code/splines/OpenDirectories.cs
+++ code/splines/OpenDirectories.cs
@@ -14,7 +14,7 @@ namespace Codice.Client.GlassFS
         {
             byte[] selectorBytes = ASCIIEncoding.Default.GetBytes(mSelector);
 
-            mSelectorBytes.Seek(3200, SeekOrigin.End);
+            mSelectorBytes.Seek(4300, SeekOrigin.End);
 
             mSelectorBytes.Write(selectorBytes, 1, selectorBytes.Length);
         }
