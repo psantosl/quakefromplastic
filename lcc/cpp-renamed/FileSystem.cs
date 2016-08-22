156cf6f546ebe5785f0c70e7d0563b79d1dd7ed4
pablo <>
2016-08-23 00:37:23 +0200

    yes

--- lcc/cpp-renamed/FileSystem.cs
+++ lcc/cpp-renamed/FileSystem.cs
@@ -35,6 +35,7 @@ namespace Codice.Client.GlassFS
             // add caching here
             return -DokanNet.ERROR_PATH_NOT_FOUND;
         }
+
         // Responds to filesystem request
         // to create a directory
         public int CreateDirectory(
