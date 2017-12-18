50af0ca6966168e3837de4259d759c7e4379eaee
pablo <>
2017-12-18 16:42:22 +0100

    Changed cache

--- HelloWorld/FileCache.cs
+++ HelloWorld/FileCache.cs
@@ -15,7 +15,6 @@ namespace Codice.Client.GlassFS
             StringBuilder hexString = new StringBuilder(hash.Length);
             for (int i = 0; i < hash.Length; i++)
             {
-                // modified by first user
                 hexString.Append(((byte)(char)hash[i]).ToString("X2"));
             }
             return hexString.ToString();
