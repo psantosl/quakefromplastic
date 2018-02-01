9868a50797d1f8a67c0bca6c0f2a6f4fd54a897b
pablo <>
2018-02-01 09:06:51 +0100

    checkin from quakecode1

--- code/renderer/tr_bsp.c
+++ code/renderer/tr_bsp.c
@@ -19,7 +19,7 @@ along with Foobar; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ===========================================================================
 */
-// tr_map.c dd
+// tr_map.c changed again
 
 #include "tr_local.h"
 
