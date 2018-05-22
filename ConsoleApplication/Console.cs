161d170e7f390d9e85aafb106c5275accf516059
pablo <>
2018-05-23 01:30:11 +0200

    another modification to console

--- ConsoleApplication/Console.cs
+++ ConsoleApplication/Console.cs
@@ -14,7 +14,7 @@ namespace System
 {
     public static class Console
     {
-        private const int DefaultConsoleBufferSize = 2020; // default size of buffer used in stream readers/writers
+        private const int DefaultConsoleBufferSize = 2021; // default size of buffer used in stream readers/writers
         private static readonly object InternalSyncObject = new object(); // for synchronizing changing of Console's static fields
         private static TextReader s_in; 
         private static TextWriter s_out, s_error;
