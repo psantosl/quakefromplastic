24fba1127c24ea4716be3c9621140bca6ae34e09
pablo <>
2018-10-16 16:40:33 +0200

    dobadu

--- code/botlib/be_aas_bspq3.c
+++ code/botlib/be_aas_bspq3.c
@@ -22,7 +22,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ===========================================================================
 */
 
-///// changed
+///// changed dooooooooooobadu
 
 #include "../game/q_shared.h"
 #include "l_memory.h"
